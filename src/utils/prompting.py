e3deafbf75c3440fba456b4255edd833e2b3efbb
Paillat <me@paillat.dev>
2024-02-15 17:54:21 +0100

    Fix dict key

--- src/utils/prompting.py
+++ src/utils/prompting.py
@@ -2,13 +2,16 @@ import yaml
 import os
 from typing import TypedDict
 
+
 class Prompt(TypedDict):
     system: str
     chat: str
 
-def get_prompt(name, *, location = "src/chore/prompts") -> Prompt:
+
+def get_prompt(name, *, location="src/chore/prompts") -> tuple[str, str]:
     path = os.path.join(os.getcwd(), location, f"{name}.yaml")
     if not os.path.exists(path):
         raise FileNotFoundError(f"Prompt file {path} does not exist.")
     with open(path, "r") as file:
-        return yaml.safe_load(file)
+        prompt: Prompt = yaml.safe_load(file)
+    return prompt["system"], prompt["chat"]
