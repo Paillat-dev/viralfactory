e8cf4bad504e321e13a06898fdc8223fe3d39c09
Paillat <me@paillat.dev>
2024-02-15 17:53:27 +0100

    Formatting

--- src/engines/LLMEngine/BaseLLMEngine.py
+++ src/engines/LLMEngine/BaseLLMEngine.py
@@ -3,8 +3,17 @@ from ..BaseEngine import BaseEngine
 
 import openai
 
-class BaseLLMEngine(BaseEngine):
 
+class BaseLLMEngine(BaseEngine):
     @abstractmethod
-    def generate(self, system_prompt: str, chat_prompt: str, max_tokens: int, temperature: float, top_p: float, frequency_penalty: float, presence_penalty: float) -> str | dict:
+    def generate(
+        self,
+        system_prompt: str,
+        chat_prompt: str,
+        max_tokens: int,
+        temperature: float,
+        top_p: float,
+        frequency_penalty: float,
+        presence_penalty: float,
+    ) -> str | dict:
         pass
