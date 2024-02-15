dee544a26cc2d122ccc0fcd930ecfebd63090ab9
Paillat <me@paillat.dev>
2024-02-15 17:52:47 +0100

    Add new engines and fix naming

--- src/engines/__init__.py
+++ src/engines/__init__.py
@@ -4,7 +4,11 @@ from . import ScriptEngine
 from . import LLMEngine
 
 ENGINES = {
-    "LLMEngine": [LLMEngine.OpenaiLLMEngine],
+    "SimpleLLMEngine": [LLMEngine.OpenaiLLMEngine, LLMEngine.AnthropicLLMEngine],
+    "PowerfulLLMEngine": [LLMEngine.OpenaiLLMEngine, LLMEngine.AnthropicLLMEngine],
     "TTSEngine": [TTSEngine.CoquiTTSEngine, TTSEngine.ElevenLabsTTSEngine],
-    "ScriptEngine": [ScriptEngine.ShowerThoughtsScriptEngine, ScriptEngine.CustomScriptEngine],
+    "ScriptEngine": [
+        ScriptEngine.ShowerThoughtsScriptEngine,
+        ScriptEngine.CustomScriptEngine,
+    ],
 }
