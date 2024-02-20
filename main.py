49857b7be33b3a4168fdde7031825aa6157cfd2f
Paillat <me@paillat.dev>
2024-02-20 14:54:05 +0100

    Add database initialization

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from ui import GenerateUI
 from dotenv import load_dotenv
+from src.models import init_db
 
 load_dotenv()
 
 if __name__ == "__main__":
+    init_db()
     ui_generator = GenerateUI()
     ui_generator.launch_ui()
