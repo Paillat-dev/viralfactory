0189d26f61ba4d4f24d590ac83da9c32334809b7
Paillat <me@paillat.dev>
2024-02-22 15:15:17 +0100

    Refactor get_metadata method signature in BaseMetadataEngine.py

--- src/engines/MetadataEngine/BaseMetadataEngine.py
+++ src/engines/MetadataEngine/BaseMetadataEngine.py
@@ -4,15 +4,10 @@ from typing import TypedDict
 from .. import BaseEngine
 
 
-class MetadataEngineSettings(TypedDict):
-    title: str
-    description: str
-
-
 class BaseMetadataEngine(BaseEngine):
     def __init__(self, **kwargs) -> None:
         ...
 
     @abstractmethod
-    def get_metadata(self, input: str) -> MetadataEngineSettings:
+    def get_metadata(self) -> None:
         ...
