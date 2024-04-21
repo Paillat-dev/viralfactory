a2c6823e8906a460fb68ac92a8a6ba62b6a1db6e
Paillat <me@paillat.dev>
2024-04-21 21:52:14 +0200

    :label: Add types and function signatures

--- src/engines/CaptioningEngine/BaseCaptioningEngine.py
+++ src/engines/CaptioningEngine/BaseCaptioningEngine.py
@@ -1,3 +1,4 @@
+import moviepy as mp
 from abc import abstractmethod
 
 from ..BaseEngine import BaseEngine
@@ -5,5 +6,4 @@ from ..BaseEngine import BaseEngine
 
 class BaseCaptioningEngine(BaseEngine):
     @abstractmethod
-    def get_captions(self) -> None:
-        ...
+    def get_captions(self, words: list[dict[str, str]] = None) -> list[mp.TextClip]: ...
